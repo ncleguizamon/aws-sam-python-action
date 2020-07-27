919f5b1e4891d98ded06aea131527d5f7aab67cf
ncleguizamon <nleguizamon@cobiscorp.com>
2020-07-26 23:41:10 -0500

    test files

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 set
 
 rm *.toml
-ls -all 
+ls -all /
 sam package --template-file $INPUT_TEMPLATEFILE --output-template-file packaged.yaml --s3-bucket $INPUT_S3_BUCKET
+ls -all /
 sam deploy --template-file packaged.yaml --stack-name $INPUT_STACK --capabilities $INPUT_CAPABILITIES --region $INPUT_AWS_DEFAULT_REGION --parameter-overrides $INPUT_PARAMETER_OVERRIDES
