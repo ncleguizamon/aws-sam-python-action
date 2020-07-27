45d892fc9d6b96d9a71f3f855d7666a329986eef
ncleguizamon <nleguizamon@cobiscorp.com>
2020-07-27 00:14:49 -0500

    test files

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ rm *.toml
 
 echo "GITHUB_WORKSPACE"
 pwd 
-ls -all $RUNNER_WORKSPACE
+ls -all /github/home
 sam package --template-file $INPUT_TEMPLATEFILE --output-template-file packaged.yaml --s3-bucket $INPUT_S3_BUCKET 
 
 sam deploy --template-file packaged.yaml --stack-name $INPUT_STACK --capabilities $INPUT_CAPABILITIES --region $INPUT_AWS_DEFAULT_REGION --parameter-overrides $INPUT_PARAMETER_OVERRIDES
