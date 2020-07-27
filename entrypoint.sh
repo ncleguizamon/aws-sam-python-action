427597f00e722aa71f66e348d9bb8355cef55553
ncleguizamon <nleguizamon@cobiscorp.com>
2020-07-26 23:30:31 -0500

    update environment variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@ set
 
 rm *.toml
 ls -all 
-sam package --template-file $TEMPLATEFILE --output-template-file packaged.yaml --s3-bucket $S3_BUCKET
-sam deploy --template-file packaged.yaml --stack-name $STACK --capabilities $CAPABILITIES --region $AWS_DEFAULT_REGION --parameter-overrides $PARAMETER_OVERRIDES
+sam package --template-file $INPUT_TEMPLATEFILE --output-template-file packaged.yaml --s3-bucket $INPUT_S3_BUCKET
+sam deploy --template-file packaged.yaml --stack-name $INPUT_STACK --capabilities $INPUT_CAPABILITIES --region $INPUT_AWS_DEFAULT_REGION --parameter-overrides $INPUT_PARAMETER_OVERRIDES
