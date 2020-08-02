ff69d7d6803e800f144fb80f7e54f1c2e352033b
ncleguizamon <nleguizamon@cobiscorp.com>
2020-08-01 21:53:55 -0500

    update entripoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,4 @@ pwd
 ls -all $GITHUB_WORKSPACE
 sam package --template-file $INPUT_TEMPLATEFILE --output-template-file packaged.yaml --s3-bucket $INPUT_S3_BUCKET 
 ls -all $GITHUB_WORKSPACE
-sam deploy --template-file packaged.yaml --stack-name $INPUT_STACK --capabilities $INPUT_CAPABILITIES --region $INPUT_AWS_DEFAULT_REGION --parameter-overrides $INPUT_PARAMETER_OVERRIDES
+sam deploy --no-fail-on-empty-changeset --template-file packaged.yaml --stack-name $INPUT_STACK --capabilities $INPUT_CAPABILITIES --region $INPUT_AWS_DEFAULT_REGION --parameter-overrides $INPUT_PARAMETER_OVERRIDES
