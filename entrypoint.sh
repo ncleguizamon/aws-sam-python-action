94e1c7e316db514d2073c88da5e578a972162814
Nestor Camilo Leguizamón <ncleguizamon3@misena.edu.co>
2020-08-01 20:58:53 -0500

    update test

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ rm *.toml
 
 echo "GITHUB_WORKSPACE"
 pwd 
-ls -all /github/home
+ls -all $GITHUB_WORKSPACE
 sam package --template-file $INPUT_TEMPLATEFILE --output-template-file packaged.yaml --s3-bucket $INPUT_S3_BUCKET 
-
+ls -all $GITHUB_WORKSPACE
 sam deploy --template-file packaged.yaml --stack-name $INPUT_STACK --capabilities $INPUT_CAPABILITIES --region $INPUT_AWS_DEFAULT_REGION --parameter-overrides $INPUT_PARAMETER_OVERRIDES
